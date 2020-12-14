32f0f7f932c5e1a15fb2b4ba0a8d4dcf8f1f7271
slemeshaev <lemeshaevs@gmail.com>
2020-12-14 21:29:47 +0300

    Files for project

---
+++ FocusTV/FocusTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FocusTV
+//
+//  Created by Станислав Лемешаев on 14.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
